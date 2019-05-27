81ee3e25b9bf8eb4734586f4643604fb32e4e9ab
Panupong <5635512110@psu.ac.th>
2019-05-27 15:46:36 +0700

    add scope and life cycle

--- src/com/panupongdeve/springdemo/TrackCoach.java
+++ src/com/panupongdeve/springdemo/TrackCoach.java
@@ -18,5 +18,20 @@ public class TrackCoach implements Coach {
 		// TODO Auto-generated method stub
 		return this.fortuneService.getFortune();
 	}
+	
+	// add an init method
+	public void doMyStartupStuff() {
+		System.out.println("TrackCoach: inside method doMyStartupStuff");
+		this.HelloWorld();
+	}
+	
+	public void HelloWorld() {
+		System.out.println("Hello MEOW!!!");
+	}
+	
+	// add a destroy method
+	public void doMyCleanupStuffYoYo() {
+		System.out.println("TrackCoach: inside method doMyCleanuoStuffYoYo");
+	}
 
 }
